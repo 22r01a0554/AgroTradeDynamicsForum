b8eda4532f2d9af9382483e61770e59c28dd3605
22r01a0554 <148076637+22r01a0554@users.noreply.github.com>
2024-11-10 01:31:47 +0530

    Create Buy.js
    Creating Buy.js containing all the products for sale which are uploaded by the farmer.

---
+++ src/components/Buy.js
@@ -0,0 +1,27 @@
+import React,{useContext,useEffect} from 'react'
+import postContext from '../context/posts/postContext';
+import Postitem from "./Postitem"
+const Buy = () => {
+const context=useContext(postContext);
+const {posts,getAllPosts}=context;
+useEffect(() => {
+    getAllPosts(); // Call getAllPosts to fetch all products
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+}, []); 
+  return (
+   <>
+    <div className="row my-3 mx-2">
+    <h2>All Products</h2>
+    <div className="container">
+        {posts?.length === 0 && 'No products to display'}
+    </div>
+    {posts && posts.map((post) => {
+        return <Postitem key={post._id} post={post} />
+    })}
+    </div>
+    </>
+
+  )
+}
+
+export default Buy
