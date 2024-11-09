597fb709ccf4b5d83196ac0debbd3ed82cdb9e51
22r01a0554 <148076637+22r01a0554@users.noreply.github.com>
2024-11-10 02:08:15 +0530

    Create index.js
    Create index.js containing signup and login for the main website  page.

---
+++ src/components/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import IndexCarousal from './IndexCarousal';
+import { Link } from "react-router-dom";
+import styles from './index.module.css';
+export default function Index() {
+    const Images = [
+        "/img/CF1.jpeg",
+        "/img/home3.jpeg",
+        "/img/a5.png",
+        "/img/r(1).jpg",
+        "/img/r(2).jpg",
+        "/img/home1.jpg",
+        "/img/CF1.jpeg",
+        "/img/home3.jpeg",
+        "/img/a5.png"
+    ];
+    return (
+        <div className={styles.bg} style={{ backgroundImage: `url('/img/bgimage1.jpg')` }}>
+            <header className={styles.headerContainer}>
+                <div className={styles.signupContainer}>
+                    <Link to="/signup"><img src="/img/sign-up.png" alt="signup"/></Link>
+                    <Link to="/login"><img src="/img/login.svg" alt="login"/></Link>
+                </div>
+            </header>
+            <div className={styles.textContainer}>
+                <h1 id={styles.animatedText}>AGRO TRADE DYNAMICS FORUM</h1>
+            </div>
+            <div className={styles.carouselContainer}>
+                <IndexCarousal images={Images} />
+            </div>
+        </div>
+    );
+}
