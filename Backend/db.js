b8de509a2cd5268f333dccde9e152d32de254ff1
22r01a0554 <148076637+22r01a0554@users.noreply.github.com>
2024-11-09 19:09:54 +0530

    Create db.js
    MongoDB Connection Created Successfully

---
+++ Backend/db.js
@@ -0,0 +1,8 @@
+const mongoose=require('mongoose');
+const mongoURI="mongodb://127.0.0.1:27017/AgroTradeDynamicsForum"
+// const mongoURI="mongodb://127.0.0.1:27017"
+const connectToMongo= async()=>{
+    await mongoose.connect(mongoURI)
+    console.log("Connected to Mongo Successfully");
+}
+module.exports=connectToMongo
