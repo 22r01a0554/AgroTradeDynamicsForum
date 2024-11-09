5307d8395fafdf074b3dd6a613b7daa07cd6dcbe
22r01a0554 <148076637+22r01a0554@users.noreply.github.com>
2024-11-09 21:04:47 +0530

    Create Video.js
    Successfully created model schema for uploading agriculture videos

---
+++ Backend/models/Video.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const videoSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    mobilenumber: {
+        type: Number,
+        required: true
+    },
+    socialmedialinks: {
+        type: String
+    },
+    address: {
+        type: String
+    },
+    videoFilename: {
+        type: String,
+        required: true
+    },
+    videoSize: {
+        type: Number,
+        required: true
+    },
+    imageFilename: {
+        type: String
+    },
+    imageSize: {
+        type: Number
+    },
+    dateUploaded: {
+        type: Date,
+        default: Date.now
+    },
+    bio: {
+        type: String
+    },
+    website: {
+        type: String
+    },
+    experience: {
+        type: String
+    },
+    affiliation: {
+        type: String
+    }
+});
+
+const Video = mongoose.model('Video', videoSchema);
+module.exports = Video;
